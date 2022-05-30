c50a56d9656d8139c59a54128f3ed418a20e37cb
npee <jmc4664@gmail.com>
2022-05-30 22:39:51 +0900

    jdbc template findById, update 구현

--- src/main/java/io/npee/itemservice/repository/jdbctempate/JdbcTemplateItemRepositoryV1.java
+++ src/main/java/io/npee/itemservice/repository/jdbctempate/JdbcTemplateItemRepositoryV1.java
@@ -8,7 +8,9 @@ import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 import javax.sql.DataSource;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
@@ -44,16 +46,34 @@ public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
 
     @Override
     public void update(Long itemId, ItemUpdateDto updateParam) {
-
+        String sql = "update item set item_name = ?, price = ?, quantity = ? where id = ?";
+        template.update(sql, updateParam.getItemName(), updateParam.getPrice(), updateParam.getQuantity(), itemId);
     }
 
     @Override
     public Optional<Item> findById(Long id) {
-        return Optional.empty();
+        String sql = "select id, item_name, price, quantity from item where id = ?";
+        try {
+            Item item = template.queryForObject(sql, itemRowMapper(), id);
+            return Optional.of(item);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Item> findAll(ItemSearchCond cond) {
         return null;
+    }
+
+    private RowMapper<Item> itemRowMapper() {
+        return ((rs, rowNum) -> {
+            Item item = new Item();
+            item.setId(rs.getLong("id"));
+            item.setItemName(rs.getString("item_name"));
+            item.setPrice(rs.getInt("price"));
+            item.setQuantity(rs.getInt("quantity"));
+            return item;
+        });
     }
 }
