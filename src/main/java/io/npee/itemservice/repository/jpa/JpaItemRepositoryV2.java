7e7126ca6214446edd19935228586b916f79dc58
npee <jmc4664@gmail.com>
2022-06-11 16:38:19 +0900

    JpaRepositoryV2 작성

---
+++ src/main/java/io/npee/itemservice/repository/jpa/JpaItemRepositoryV2.java
@@ -0,0 +1,49 @@
+package io.npee.itemservice.repository.jpa;
+
+import io.npee.itemservice.domain.Item;
+import io.npee.itemservice.repository.ItemRepository;
+import io.npee.itemservice.repository.ItemSearchCond;
+import io.npee.itemservice.repository.ItemUpdateDto;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
+
+@RequiredArgsConstructor
+public class JpaItemRepositoryV2 implements ItemRepository {
+
+    private final SpringDataJpaItemRepository repository;
+
+    @Override
+    public Item save(Item item) {
+        return repository.save(item);
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = repository.findById(itemId).orElseThrow();
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+        if (StringUtils.hasText(itemName) && maxPrice != null) {
+            return repository.findItems("%" + itemName + "%", maxPrice);
+        } else if (StringUtils.hasText(itemName)) {
+            return repository.findByItemNameLike("%" + itemName + "%");
+        } else if (maxPrice != null) {
+            return repository.findByPriceLessThanEqual(maxPrice);
+        } else {
+            return repository.findAll();
+        }
+    }
+}
